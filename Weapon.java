86ac4daf4885c9a9a5974cf759bb69b6d2acfd75
Dan45RS <67072831+Dan45RS@users.noreply.github.com>
2023-11-15 11:36:53 -0600

    Weapon now similar to Item

--- Weapon.java
+++ Weapon.java
@@ -1,9 +1,11 @@
 package Alien_Temple;
 
-class Weapon
+class Weapon extends Item
 {
+    /*
     private Item weaponName;
     private Item weaponDescription;
+    */
     private String weaponStats;
     private int dmgOutput;
 
@@ -25,5 +27,12 @@ class Weapon
     public int getDmgOutput()
     {
         return dmgOutput;
+    }
+
+    public Weapon(String itemName, String itemDescription, String weaponStats, int dmgOutput)
+    {
+        super(itemName, itemDescription);
+        this.weaponStats = weaponStats;
+        this.dmgOutput = dmgOutput;
     }
 }
